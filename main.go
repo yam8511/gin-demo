56c920ab6e320cb7d1b3d654d6b331ff8905026c
Zuolar <yam8511@gmail.com>
2017-08-31 00:54:19 +0800

    add signal and flag

--- main.go
+++ main.go
@@ -2,15 +2,18 @@ package main
 
 import (
 	"encoding/json"
+	"flag"
 	"fmt"
 	"log"
 	"net/http"
 	"os"
+	"os/signal"
 	"strconv"
+	"syscall"
 
 	"github.com/gin-gonic/gin"
 	"github.com/joho/godotenv"
-	_ "github.com/joho/godotenv/autoload"
+	// _ "github.com/joho/godotenv/autoload" ----> uncomment will autoload .env file
 	"github.com/tucnak/telebot"
 )
 
@@ -33,8 +36,17 @@ var AdminChat telebot.Chat
 var Bot *telebot.Bot
 
 func main() {
+	/// 設定參數
+	envFile := flag.String("e", "", "指定 env 檔案名稱")
+	flag.Parse()
+
 	/// 讀取 ENV 設定檔
-	err := godotenv.Load()
+	var err interface{}
+	if *envFile == "" {
+		err = godotenv.Load()
+	} else {
+		err = godotenv.Load(*envFile)
+	}
 	CheckErrFatal(err, "讀取 .env 錯誤")
 
 	/// 存取 Env 變數
@@ -48,22 +60,14 @@ func main() {
 	ChatID := int64(cahtid)
 
 	/// Telegram - 伺服器關掉時通知
-	AdminChat := &telebot.Chat{ID: ChatID}
+	AdminChat = telebot.Chat{ID: ChatID}
 	Bot, err = telebot.NewBot(BotToken)
 	CheckErrFatal(err, "NewBot Error", err)
-	defer func(BotToken string, ChatID int64) {
-		err := recover()
-		message := HOST + " 伺服器關閉了!  "
-		if err != nil {
-			message += fmt.Sprintf("(%v)", err)
-		}
-		Bot.SendMessage(AdminChat, message, nil)
-	}(BotToken, ChatID)
 
-	// WebSocket Setting
+	/// WebSocket Setting
 	wsserver := SocketInit()
 
-	// // Set Gin Mode
+	/// Set Gin Mode
 	if GinMode == "release" {
 		gin.SetMode(gin.ReleaseMode)
 	} else {
@@ -76,7 +80,7 @@ func main() {
 	r.LoadHTMLGlob("asset/*")
 	r.GET("/ping", pong)
 
-	// Socket Demo
+	/// Socket Demo
 	r.GET("/socket.io/", func(c *gin.Context) {
 		c.Writer.Header().Set("Access-Control-Allow-Origin", AccessOrigin)
 		c.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
@@ -86,13 +90,58 @@ func main() {
 		c.HTML(http.StatusOK, "chat.html", nil)
 	})
 
-	// GraphQL Demo
+	/// GraphQL Demo
 	todoInit()
 	r.Any("/graphql", GraphQLHandle)
 	r.GET("/graphiql", GraphIQLHandle)
 
-	// Start Server
-	r.Run(PORT)
+	/// 宣告系統信號
+	sigs := make(chan os.Signal, 1)
+	exit := make(chan interface{})
+	signal.Notify(sigs, syscall.SIGINT, syscall.SIGTERM)
+
+	/// 宣告設定 Server
+	server := &http.Server{
+		Addr:    PORT,
+		Handler: r,
+		// ReadTimeout:    10 * time.Second,
+		// WriteTimeout:   10 * time.Second,
+		// MaxHeaderBytes: 1 << 20,
+	}
+
+	go func() {
+		/// 監聽伺服器
+		log.Println("Server Listening on ", HOST+PORT)
+		err = server.ListenAndServe()
+
+		// 如果監聽發生錯誤，通知系統人員
+		if err != nil {
+			if fmt.Sprintf("%v", err) != "http: Server closed" {
+				log.Println("Server Error", err)
+				NoticeSystemManager(err)
+				close(exit)
+			}
+		}
+	}()
+
+	/// 設置 Ctrl + C 機制
+	go func() {
+		log.Println("結束程式請按 [Ctrl + C]")
+		// 等待 Ctrl + C 的信號
+		receivedSignel := <-sigs
+
+		// 關閉伺服器
+		err = server.Close()
+		// 通知伺服器被關閉
+		NoticeSystemManager(err)
+
+		// 離開程式
+		exit <- receivedSignel
+	}()
+
+	/// 等待 Ctrl + C 結束程式
+	log.Printf("\nSignal: %v", <-exit)
+	log.Println("程式結束")
 }
 
 // CheckErrFatal : 確認錯誤，如果有錯誤則結束程式
@@ -101,8 +150,19 @@ func CheckErrFatal(err interface{}, msg ...interface{}) {
 		if len(msg) == 0 {
 			log.Fatalln(err)
 		}
-		log.Fatalln(msg)
+		log.Fatalln(msg, err)
 	}
+}
+
+// NoticeSystemManager : 通知系統人員
+func NoticeSystemManager(err interface{}) {
+	message := HOST + " 伺服器關閉了!  "
+	if err != nil {
+		message += fmt.Sprintf("(%v)", err)
+	} else {
+		message += "(手動關閉)"
+	}
+	Bot.SendMessage(AdminChat, message, nil)
 }
 
 func pong(c *gin.Context) {
